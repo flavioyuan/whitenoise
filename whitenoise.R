698bd2c96daf99476f978f4caf900c1f5e893d7e
flavioyuan <fyuangouvea@gmail.com>
2024-04-28 20:48:40 -0300

    Definig weight phi vector

--- whitenoise.R
+++ whitenoise.R
@@ -3,3 +3,6 @@ white_noise <- rnorm(n = 1000, mean = 0, sd = 1)
 
 # Plot the white noise
 plot(white_noise, type = "l", main = "White Noise", xlab = "Time", ylab = "Amplitude")
+
+phi0 <- 1
+phi <- runif(n=100, min=0, max=100)
