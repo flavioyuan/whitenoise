317b21322352df99bd01a642b5e55a82fe0aa2ab
Flavio Yuan <fyuangouvea@gmail.com>
2024-06-02 00:08:33 -0300

    renaming whitenoise to a

--- whitenoise.R
+++ whitenoise.R
@@ -1,8 +1,8 @@
 # Generate white noise
-white_noise <- rnorm(n = 1000, mean = 0, sd = 1)
+a <- rnorm(n = 1000, mean = 0, sd = 1)
 
 # Plot the white noise
-plot(white_noise, type = "l", main = "White Noise", xlab = "Time", ylab = "Amplitude")
+plot(a, type = "l", main = "White Noise", xlab = "Time", ylab = "Amplitude")
 
 # Defining parameters
 
