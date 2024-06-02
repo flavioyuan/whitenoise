dfff88bd3e39d494f8af7ee87373109b1c036878
Flavio Yuan <fyuangouvea@gmail.com>
2024-06-01 23:53:34 -0300

    Add some comments

--- whitenoise.R
+++ whitenoise.R
@@ -4,5 +4,7 @@ white_noise <- rnorm(n = 1000, mean = 0, sd = 1)
 # Plot the white noise
 plot(white_noise, type = "l", main = "White Noise", xlab = "Time", ylab = "Amplitude")
 
+# Defining parameters
+
 phi0 <- 1
 phi <- runif(n=100, min=0, max=100)
