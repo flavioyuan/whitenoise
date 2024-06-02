2f79224e87ac215196ae10dd2b3c97a007214efd
Flavio Yuan <fyuangouvea@gmail.com>
2024-06-02 10:35:45 -0300

    Add libraries and configure time serie parameters

--- whitenoise.R
+++ whitenoise.R
@@ -1,10 +1,17 @@
-# Generate white noise
-white_noise <- rnorm(n = 1000, mean = 0, sd = 1)
+require(tidyverse)
+require(tibble)
+require(ggplot2)
 
-# Plot the white noise
-plot(white_noise, type = "l", main = "White Noise", xlab = "Time", ylab = "Amplitude")
-
-# Defining parameters
-
+# Parameters
+size_serie <- 1000
+mean_wn <- 0
+sd_wn <- 1
 phi0 <- 1
 phi1 <- runif(n=1, min=-0.99, max=0.99)
+dates_vector <- seq.Date(from = as.Date("01-01-2000"), by = "day", length.out = size_serie)
+
+# Generate white noise
+a_t <- rnorm(n = size_serie, mean = mean_wn, sd = sd_wn)
+
+# Plot the white noise
+plot(a_t, type = "l", main = "White Noise", xlab = "Time", ylab = "Amplitude")
