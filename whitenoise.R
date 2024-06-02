38cecc27c993806972fed7063b33f18a8e72ddf3
Flavio Yuan <fyuangouvea@gmail.com>
2024-06-02 00:23:11 -0300

    abs value phi1 less tha or equal 0.99

--- whitenoise.R
+++ whitenoise.R
@@ -7,4 +7,4 @@ plot(white_noise, type = "l", main = "White Noise", xlab = "Time", ylab = "Ampli
 # Defining parameters
 
 phi0 <- 1
-phi1 <- runif(n=1, min=-1, max=1)
+phi1 <- runif(n=1, min=-0.99, max=0.99)
