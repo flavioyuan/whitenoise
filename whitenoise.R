400b87ff3a931a010f32db94e1305b2504a329d4
Flavio Yuan <fyuangouvea@gmail.com>
2024-06-02 10:48:41 -0300

    Create tibble to store time serie and configure ggplot

--- whitenoise.R
+++ whitenoise.R
@@ -15,3 +15,12 @@ a_t <- rnorm(n = size_serie, mean = mean_wn, sd = sd_wn)
 
 # Plot the white noise
 plot(a_t, type = "l", main = "White Noise", xlab = "Time", ylab = "Amplitude")
+
+# Matrix
+ar <- tibble(dates_vector, a_t)
+
+# Plot
+ggplot(ar, aes(x = dates_vector, y = a_t)) + 
+    geom_line() +
+    labs(title = "Time Serie", x = "Date", y = "a_t") +
+    theme_minimal()
